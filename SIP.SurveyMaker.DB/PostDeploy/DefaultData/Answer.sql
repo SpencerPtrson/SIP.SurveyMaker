40703b34d5a57ef7d8bed4e870564cb9e5b45cd2
Spencer Peterson <700230728@fvtc.edu>
2023-01-24 13:12:30 -0600

    Added some basic answers (not finished with answers / default data). Manually changed remote repository connection to reflect name change.

--- SIP.SurveyMaker.DB/PostDeploy/DefaultData/Answer.sql
+++ SIP.SurveyMaker.DB/PostDeploy/DefaultData/Answer.sql
@@ -1,1 +1,19 @@
-﻿
+﻿BEGIN
+	INSERT INTO dbo.tblAnswer (Id, Answer)
+	VALUES
+		(NEWID(), 'I am currently employed.'),
+		(NEWID(), 'I am not currently employed.'),
+
+		(NEWID(), 'What is your opinion of our company?'),
+		(NEWID(), 'Positive opinion'),
+		(NEWID(), 'Negative opinion'),
+		(NEWID(), 'Ambivalent / Unaware'),
+
+
+
+		(NEWID(), 'How likely are you to use our services again?'),
+		(NEWID(), 'I feel valued at my company'),
+
+
+		(NEWID(), 'I feel valued at my company')
+END
