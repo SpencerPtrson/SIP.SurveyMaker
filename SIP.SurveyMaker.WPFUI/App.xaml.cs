0cf2279f837dc9b1ddf3ad6fcdb3967b859f7b00
Spencer Peterson <700230728@fvtc.edu>
2023-02-23 16:36:49 -0600

    Added nuget packages, json file, and custom control code

--- SIP.SurveyMaker.WPFUI/App.xaml.cs
+++ SIP.SurveyMaker.WPFUI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,28 @@ namespace SIP.SurveyMaker.WPFUI
     /// </summary>
     public partial class App : Application
     {
+        ServiceProvider serviceProvider;
+        public static IConfiguration Configuration;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configSettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+            Configuration = configSettings;
+            services.AddSingleton<VehicleList>();
+        }
+
+        private void OnStartUp(object sender, StartupEventArgs e)
+        {
+            var VehicleList = serviceProvider.GetService<VehicleList>();
+            VehicleList.Show();
+        }
     }
 }
---
+++ SIP.SurveyMaker.WPFUI/ucMaintainQA.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SIP.SurveyMaker.BL;
+using SIP.SurveyMaker.BL.Models;
+
+namespace SIP.SurveyMaker.WPFUI
+{
+    public enum ControlMode : int
+    {
+        Question = 0,
+        Answer = 1
+    }
+
+    /// <summary>
+    /// Interaction logic for ucMaintainQA.xaml
+    /// </summary>
+    public partial class ucMaintainQA : UserControl
+    {
+        ControlMode controlMode;
+        public List<Question> Questions { get; set; }
+        public List<Answer> Answers { get; set; }
+
+        public ucMaintainQA()
+        {
+            InitializeComponent();
+        }
+
+        public ucMaintainQA(ControlMode controlMode, Guid selectedId)
+        {
+            InitializeComponent();
+            this.controlMode = controlMode;
+            Reload();
+            cboText.SelectedValue = selectedId;
+        }
+
+        public ucMaintainQA(ControlMode controlMode, int selectedId)
+        {
+            InitializeComponent();
+            this.controlMode = controlMode;
+            Reload();
+            cboText.SelectedValue = selectedId;
+        }
+
+        // Get control mode (whatever attribute the control is meant to get data for) and load appropriate data
+        private async void Reload()
+        {
+            cboText.ItemsSource = null;
+
+            switch (controlMode) // Set ItemsSource depending on ControlMode
+            {
+                case ControlMode.Question:
+                    Questions = (List<Question>)await QuestionManager.Load();
+                    cboText.ItemsSource = Questions;
+                    break;
+                case ControlMode.Answer:
+                    Answers = (List<Answer>)await AnswerManager.Load();
+                    cboText.ItemsSource = Answers;
+                    break;
+            }
+            cboText.DisplayMemberPath = "Text";
+            cboText.SelectedValuePath = "Id";
+        }
+    }
+}
