56f54c36a9f0ab73e6d4797cb9b1eb9761f82d96
Spencer Peterson <700230728@fvtc.edu>
2023-03-10 16:59:13 -0600

    Changed API.Test program to allow API Tests to work (Some still need work done on them)

--- SIP.SurveyMaker.API.Test/utQuestion.cs
+++ SIP.SurveyMaker.API.Test/utQuestion.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SIP.SurveyMaker.BL;
+using SIP.SurveyMaker.BL.Models;
 using System.Drawing;
 using System.Reflection;
 
@@ -13,20 +15,21 @@ namespace SIP.SurveyMaker.API.Test
     [TestClass]
     public class utQuestion : utBase
     {
-        //[TestMethod]
-        //public async Task LoadTestAsync()
-        //{
-        //    await base.LoadTestAsync<Color>();
-        //    // The base class has the assert.stuff, so it's not needed here
-        //}
+        [TestMethod]
+        public async Task LoadTestAsync()
+        {
+            await base.LoadTestAsync<Question>();
+            // The base class has the assert.stuff, so it's not needed here
+        }
 
-        //[TestMethod]
-        //public async Task InsertTestAsync()
-        //{
-        //    Color color = new Color { Description = "New Color" };
-        //    color.Code = BitConverter.ToInt32(new byte[] { 255, 0, 0, 0x00 }, 0);
-        //    await base.InsertTestAsync<Color>(color);
-        //}
+        [TestMethod]
+        public async Task InsertTestAsync()
+        {
+            List<Activation> activations = await ActivationManager.Load();
+            List<Answer> answers = await AnswerManager.Load();
+            Question question = new Question { Text = "Hello", Answers = answers, Activations = activations };
+            await base.InsertTestAsync<Question>(question);
+        }
 
         //[TestMethod]
         //public async Task UpdateTestAsync()
